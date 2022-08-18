5f4780b2cc1c96f30b5567900eb23bc37a12ecda
terry <fc.ho@hotmail.com>
2022-08-18 07:11:11 +0000

    added cookies

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+var cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -32,20 +34,39 @@ res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+    res.cookie('name', req.body.username )
+    const templateVars = { urls: urlDatabase, username: req.cookies.username };
+    res.render("urls_index",templateVars);
   });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const newLongURL = req.body.longURL;
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL
   //this is use to define the :shortURL. ,req.params come from the form action
-  //in the form ,the action="/urls/<%= id %>/delete" already give each button its own shourt url
+  //in the form ,the action="/urls/<%= id %>/delete" already give each button its own short url
   delete urlDatabase[shortURL];
   //delete the data which storing in the database so it will disappear
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username )
+  console.log('Cookies: ', req.cookies)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', req.body.username)
   res.redirect("/urls");
 });
 
@@ -65,7 +86,10 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.cookie('username', req.body.username )
+    //the edit button also res this page 
+      //in the form ,the action="/urls/<%= id %>" already give each button its own short url
+const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username };
 res.render("urls_show", templateVars);
 });
 
