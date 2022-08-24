e04f6c32c6f6e4c8f21da6af3acafee3c3759f6b
terry <fc.ho@hotmail.com>
2022-08-24 14:06:35 +0000

    added bcrypt password& pages permisson features

--- express_server.js
+++ express_server.js
@@ -6,10 +6,20 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 const findEmail = require("./helpers.js");
+const bcrypt = require("bcryptjs");
+const password = "purple-monkey-dinosaur"; // found in the req.body object
+const hashedPassword = bcrypt.hashSync(password, 10);
+
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -35,8 +45,19 @@ const generateRandomString = function() {
   return finalArray.join("");
 };
 
+const filterDatebase = function(id) {
+  let newDatabase = {};
+  const shortKeys = Object.keys(urlDatabase);
+  for (let i = 0; i < shortKeys.length; i++) {
+    if (urlDatabase[shortKeys[i]].userID === id) {
+      newDatabase[shortKeys[i]] = urlDatabase[shortKeys[i]];
+    }
+  }
+  return newDatabase;
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/login")
 });
 
 app.get("/urls.json", (req, res) => {
@@ -48,34 +69,54 @@ res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase, user_id:req.cookies.userid, email:req.cookies.userid};
-    res.render("urls_index", templateVars);
+  console.log(urlDatabase)
+  if (!req.cookies.userid) {
+    res.redirect("/login")
+  } else {
+    console.log("ID"+req.cookies.userid.id)
+    console.log(filterDatebase(req.cookies.userid.id))
+    const userDatabase = filterDatebase(req.cookies.userid.id)
+    const templateVars = { urls: userDatabase, user_id:req.cookies.userid, email:req.cookies.userid};
+    res.render("urls_index", templateVars)};
   });
 
-app.get("/urls/new", (req, res) => {
-  const templateVars = { user_id:req.cookies.userid,email:req.cookies.userid };
-  res.render("urls_new", templateVars);
+app.get("/new", (req, res) => {
+  if (req.cookies.userid) {
+  const templateVars = { user_id:req.cookies.userid,email:req.cookies.userid }
+  res.render("urls_new", templateVars)
+} else {
+  res.redirect("/login")
+}
 });
 
 app.get("/login", (req, res) => {
+  if (req.cookies.userid) {
+    res.redirect("/urls")
+  } else {
   const templateVars = { user_id:req.cookies.userid,email:req.cookies.userid };
-  res.render("urls_login", templateVars);
+  res.render("urls_login", templateVars);}
 });
 
 app.get("/register", (req, res) => {
+  if (req.cookies.userid) {
+    res.redirect("/urls")
+  } else {
   const templateVars = { user_id:req.cookies.userid,email:req.cookies.userid};
-  res.render("urls_register", templateVars);
+  res.render("urls_register", templateVars)}
 });
 
 app.post("/urls/:shortURL", (req, res) => {
+  console.log("eafffffffffffffffffffff")
   const shortURL = req.params.shortURL;
   const newLongURL = req.body.longURL;
-  urlDatabase[shortURL] = newLongURL;
+  urlDatabase[shortURL].longURL = newLongURL;
+  console.log(urlDatabase)
   res.redirect("/urls");
 });
+
 app.post("/register", (req, res) => {
   const email = req.body.email;
-  const password = req.body.password;
+  const password = bcrypt.hashSync(req.body.password, 10);
   const id = generateRandomString()
   const usersData = {
     id: id,
@@ -97,28 +138,31 @@ if (email === "" || password === "") {
 
 
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const templateVars = { user_id: req.cookies.userid };
   const shortURL = req.params.shortURL
-  //this is use to define the :shortURL. ,req.params come from the form action
-  //in the form ,the action="/urls/<%= id %>/delete" already give each button its own short url
-  delete urlDatabase[shortURL];
-  //delete the data which storing in the database so it will disappear
-  res.redirect("/urls");
+  console.log("111111: "+urlDatabase[shortURL].userID)
+  console.log("222222: "+templateVars.user_id.id)
+  if (urlDatabase[shortURL].userID === templateVars.user_id.id) {
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+  }
 });
 
 app.post("/login", (req, res) => {
 
   const email = req.body.email;
   const password = req.body.password;
+  const findUser = findEmail(email,users)
 
-if (email === "" || password === "") {
-  res.status(400).send("400: Please enter your Email and password.")
-  } else if (findEmail(email,users).email === email && findEmail(email,users).password === password) {
+if (!findUser.password||!findUser.email) {
+  res.status(403).send("403: Incorrect email or password");
+}
+if (bcrypt.compareSync(password, findUser.password) === true) {
     res.cookie ('userid',findEmail(email,users))
     res.redirect("/urls");
-  } else {
-    res.status(403).send("403: Incorrect email or password");
-  }
-
+} else {
+  res.status(403).send("403: User does not exist");
+}
 })
 
 app.post("/logout", (req, res) => {
@@ -128,26 +172,42 @@ app.post("/logout", (req, res) => {
 
 app.post("/urls", (req, res) => {
 
-  const templateVars = { shortURL: generateRandomString(), longURL: req.body.longURL};
-  //use the generated string as shotURL // set the longURL is what the user entered on the new page
+  const templateVars = { shortURL: generateRandomString(), longURL: req.body.longURL, userID: req.cookies.userid};
   const shortURL = templateVars.shortURL;
-  // give a name for it so is can be use
-  urlDatabase[shortURL] = templateVars.longURL; 
-  // adding the new data as object to database
-  res.redirect(`/urls/${shortURL}`);        
+  urlDatabase[shortURL] = { longURL: templateVars.longURL, userID: templateVars.userID.id}; 
+
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
+  const longURL = urlDatabase[req.params.id].longURL
   res.redirect(longURL);
 });
 
-
 console.log(users)
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user_id:req.cookies.userid,email:req.cookies.userid};
-  res.render("urls_show",templateVars);
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id].longURL, 
+    user_id:req.cookies.userid, 
+    email:req.cookies.userid
+  };
+  if (templateVars.user_id !== undefined && templateVars.user_id.id === urlDatabase[req.params.id].userID) {
+    console.log(templateVars.user_id.id)
+    console.log(urlDatabase[req.params.id])
+    console.log(urlDatabase[req.params.id].userID)
+  res.render("urls_show",templateVars)
+} else {
+  console.log("sfwfwefwe")
+  res.redirect("/login")
+}
 });
+
+app.get("/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  if (shortURL){
+  res.redirect("/urls");
+}});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
